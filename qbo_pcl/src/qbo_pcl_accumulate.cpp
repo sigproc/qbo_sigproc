ff418a5ce60292500a0e3f6995825f14a7fbc393
Erik Rosen <er367@cam.ac.uk>
2013-10-21 17:49:06 +0100

    qbo_pcl_accumulate now runs a voxel filter to the collection of points to prevent memory catastrophe. qbo_pcl_decimator can now filter point clouds.

--- qbo_pcl/src/qbo_pcl_accumulate.cpp
+++ qbo_pcl/src/qbo_pcl_accumulate.cpp
@@ -6,13 +6,13 @@
 #include <pcl_ros/transforms.h>
 #include <pcl/point_types.h>
 #include <iostream>
+#include <pcl/filters/voxel_grid.h>
 
 ros::Publisher pub;
-pcl::PointCloud<pcl::PointXYZ> accumulated_points;
+pcl::PointCloud<pcl::PointXYZ>::Ptr accumulated_points_ptr;
 std::string base_frame_id;
 
 void accumulate_pc(const sensor_msgs::PointCloud2 &ros_pc){
-	base_frame_id = ros_pc.header.frame_id;
 
 	pcl::PCLPointCloud2 pcl_pc2; 
 	pcl_conversions::toPCL(ros_pc, pcl_pc2);
@@ -22,28 +22,45 @@ void accumulate_pc(const sensor_msgs::PointCloud2 &ros_pc){
 	pcl::fromPCLPointCloud2(pcl_pc2, pcl_pc);
 
 	//Stitch it all up
-	accumulated_points += pcl_pc;
+	*accumulated_points_ptr += pcl_pc;
 	
+	//Convert Filter it!
+
+
+  	pcl::VoxelGrid<pcl::PointXYZ> sor;
+  	sor.setInputCloud (accumulated_points_ptr);
+  	sor.setLeafSize (0.1f, 0.1f, 0.1f);
+  	pcl::PointCloud<pcl::PointXYZ> temp;
+  	sor.filter(temp);
+
+  	*accumulated_points_ptr = temp;
+
     //Publish it
 
     // temporary PointCloud2 intermediary
     pcl::PCLPointCloud2 tmp_pc;
 
     // Convert fused from PCL native type to ROS
-    pcl::toPCLPointCloud2(accumulated_points, tmp_pc);
+    pcl::toPCLPointCloud2(*accumulated_points_ptr, tmp_pc);
     
     sensor_msgs::PointCloud2 published_pc;
     pcl_conversions::fromPCL(tmp_pc, published_pc);
 
-    published_pc.header.frame_id = base_frame_id;
+    published_pc.header = ros_pc.header;
 
     // Publish the data
     pub.publish(published_pc);
 }
 
+
 int main(int argc, char **argv){
 	ros::init(argc,argv, "point_cloud_accumulator");
 	ros::NodeHandle node;
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr new_cloud(
+		new pcl::PointCloud<pcl::PointXYZ>());
+	accumulated_points_ptr = new_cloud;
+
 	ros::Subscriber sub = node.subscribe("camera/depth/transformed_points", 1, accumulate_pc);
 
 	pub = node.advertise<sensor_msgs::PointCloud2> ("camera/depth/accumulated_points",1);
