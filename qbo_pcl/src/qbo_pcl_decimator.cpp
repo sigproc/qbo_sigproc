6cc05d9f3d858a4486ccf75637d4c5d2e7e6459a
Erik Rosen <er367@cam.ac.uk>
2013-10-29 16:14:22 +0000

    Cleaned up qbo_pcl_decimator

--- qbo_pcl/src/qbo_pcl_decimator.cpp
+++ qbo_pcl/src/qbo_pcl_decimator.cpp
@@ -9,9 +9,26 @@
 // Other
 #include <iostream>
 
-ros::Publisher pub;
 
-void decimate_pc(const sensor_msgs::PointCloud2 &rich_ros_pc){
+
+class Decimator
+{
+public:
+  Decimator(std::string input_pc_topic="camera/depth/transformed_points",std::string output_pc_topic="camera/depth/decimated_points"){
+  _sub = _node.subscribe(input_pc_topic, 1, &Decimator::decimate_pc,this);
+  _pub = _node.advertise<sensor_msgs::PointCloud2> (output_pc_topic,1);
+  ROS_INFO("Decimator initialized");
+  }
+  ~Decimator(){}
+
+  private:
+  void decimate_pc(const sensor_msgs::PointCloud2&);
+  ros::NodeHandle _node;
+  ros::Publisher _pub;
+  ros::Subscriber _sub;
+};
+
+void Decimator::decimate_pc(const sensor_msgs::PointCloud2 &rich_ros_pc){
 
 	pcl::PCLPointCloud2::Ptr rich_pcl_pc (new pcl::PCLPointCloud2());
 	pcl::PCLPointCloud2::Ptr sparse_pcl_pc (new pcl::PCLPointCloud2());
@@ -19,28 +36,26 @@ void decimate_pc(const sensor_msgs::PointCloud2 &rich_ros_pc){
 
 	pcl_conversions::toPCL(rich_ros_pc, *rich_pcl_pc);
 
-  	pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-  	sor.setInputCloud (rich_pcl_pc);
-  	sor.setLeafSize (0.1f, 0.1f, 0.1f);
-  	sor.filter(*sparse_pcl_pc);
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (rich_pcl_pc);
+  sor.setLeafSize (0.1f, 0.1f, 0.1f);
+  sor.filter(*sparse_pcl_pc);
 
-  	std::cerr << "PointCloud before filtering: " << rich_pcl_pc->width * rich_pcl_pc->height 
-    << " data points (" << pcl::getFieldsList (*rich_pcl_pc) << ")." << std::endl;
+ 	std::cout << "PointCloud before filtering: " << rich_pcl_pc->width * rich_pcl_pc->height 
+  << " data points (" << pcl::getFieldsList (*rich_pcl_pc) << ")." << std::endl;
 
-    std::cerr << "PointCloud after filtering: " << sparse_pcl_pc->width * sparse_pcl_pc->height 
-       << " data points (" << pcl::getFieldsList (*sparse_pcl_pc) << ")."<< std::endl;
+  std::cout << "PointCloud after filtering: " << sparse_pcl_pc->width * sparse_pcl_pc->height 
+  << " data points (" << pcl::getFieldsList (*sparse_pcl_pc) << ")."<< std::endl;
 
-  	pcl_conversions::fromPCL(*sparse_pcl_pc, sparse_ros_pc);
+	pcl_conversions::fromPCL(*sparse_pcl_pc, sparse_ros_pc);
 
-  	pub.publish(sparse_ros_pc);
+ 	_pub.publish(sparse_ros_pc);
 }
 
 int main(int argc, char **argv){
 	ros::init(argc,argv, "point_cloud_decimator");
-	ros::NodeHandle node;
-	ros::Subscriber sub = node.subscribe("camera/depth/transformed_points", 1, decimate_pc);
 
-	pub = node.advertise<sensor_msgs::PointCloud2> ("camera/depth/decimated_points",1);
+  Decimator myDecimator;
 
 	ros::spin();
 }
