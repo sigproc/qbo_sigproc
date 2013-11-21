e8782b571369b28124f33412523e90605a7a310f
Erik Rosen <er367@cam.ac.uk>
2013-11-21 22:27:32 +0000

    Modified transformer to not throw errors

--- qbo_pcl/src/qbo_pcl_transform.cpp
+++ qbo_pcl/src/qbo_pcl_transform.cpp
@@ -62,18 +62,41 @@ void Transformer::initParams(void){
 
 void Transformer::transformAndPublishPC(const sensor_msgs::PointCloud2 &inputCloud){
 	ROS_INFO("Transforming PC");
-	sensor_msgs::PointCloud2 output;
-	std::cout << inputCloud.header.frame_id << std::endl;
-	output.header = inputCloud.header;
-	//Get the transform matrix and do transformation here
+	pcl::PCLPointCloud2 pcl_pc2; 
+	pcl_conversions::toPCL(inputCloud, pcl_pc2);
 
-	if(!pcl_ros::transformPointCloud(_target_frame, inputCloud, output, _transform_listener)){
+	//Convert to PointCloud
+	pcl::PointCloud<pcl::PointXYZ> pcl_pc;
+	pcl::PointCloud<pcl::PointXYZ> pcl_pc_out;
+	pcl::fromPCLPointCloud2(pcl_pc2, pcl_pc);
+
+
+	sensor_msgs::PointCloud2 outputmsg;
+	std::cout << inputCloud.header.frame_id << std::endl;
+	outputmsg.header = inputCloud.header;
+	//Get the transform matrix and do transformation here
+	
+	ros::Time now = ros::Time::now();
+    _transform_listener.waitForTransform(inputCloud.header.frame_id, _target_frame,
+                              now, ros::Duration(3.0));
+
+
+	if(!pcl_ros::transformPointCloud(_target_frame, now, pcl_pc, inputCloud.header.frame_id, pcl_pc_out, _transform_listener)){
 		ROS_ERROR("Something went wrong when transforming PC");
 		return;
 	}
+	// temporary PointCloud2 intermediary
+    pcl::PCLPointCloud2 tmp_pc;
+
+    // Convert fused from PCL native type to ROS
+    pcl::toPCLPointCloud2(pcl_pc_out, tmp_pc);
+    
+    sensor_msgs::PointCloud2 published_pc;
+    pcl_conversions::fromPCL(tmp_pc, published_pc);
+
 	//Publish transformed cloud here
-	std::cout << output.header.frame_id << std::endl;
-	_pub.publish(output);
+	std::cout << published_pc.header.frame_id << std::endl;
+	_pub.publish(published_pc);
 }
 
 int main(int argc, char **argv)
